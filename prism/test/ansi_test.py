380c040d1713dddbab811f57221ba74d8dad1251
Peter Hillerström <peter.hillerstrom@gmail.com>
2012-07-12 03:43:04 +0300

    Changed tests to use pytest instead of the unittest module.

--- prism/test/ansi_test.py
+++ prism/test/ansi_test.py
@@ -7,33 +7,27 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
-from unittest import TestCase
-from ..ansi import *
+import pytest
+from ..ansi import code, colour, colourcode
 
+@pytest.mark.parametrize(("code", "name"), [
+    # invalid
+    ['', None],
+    ['', ''],
+    ['', 'invalid'],
+    ['', 'bright invalid'],
+    # valid
+    [30, 'BLACK'],
+    [31, 'red'],
+    # ...
+    [37, 'white'],
+    # bright
+    [91, 'BRIGHT RED'],
+    [96, 'foo bright cyan red bar'],
+])
+def test_colourcode(code, name):
+    assert code == colourcode(name)
+    assert code + 10 if isinstance(code, int) else '' == colourcode(name, back=True)
 
-class AnsiTest(TestCase):
-
-    def colourcode_test(self):
-    	test_data = [
-    		# invalid
-    		['', None],
-    		['', ''],
-    		['', 'invalid'],
-    		['', 'bright invalid'],
-    		# valid
-    		[30, 'BLACK'],
-    		[31, 'red'],
-    		# ...
-    		[37, 'white'],
-    		# bright
-    		[91, 'BRIGHT RED'],
-    		[96, 'foo bright cyan red bar'],
-    	]
-
-    	for code, name in test_data:
-	        self.assertEquals(code, colourcode(name))
-        	self.assertEquals(code + 10 if isinstance(code, int) else '', colourcode(name, back=True))
-
-
-    def colour_test(self):
-    	self.assertEquals('\x1b[31;m', colour('RED'))
+def test_colour():
+    assert '\x1b[31;m' == colour('RED')
--- prism/test/grep_test.py
+++ prism/test/grep_test.py
@@ -7,21 +7,15 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
-from unittest import TestCase
-from ..grep import *
+import pytest
+from ..grep import pattern, search
 
+def log_lines():
+    return [
+        "[Sun Apr 08 12:51:52 2012] [notice] Digest: done",
+        "[Mon Jul 11 09:26:13 2011] Error: [client ::1] File does not exist: /Library/WebServer/Documents/favicon.ico",
+    ]
 
-class GrepTest(TestCase):
-
-    def match_test(self):
-    	lines = [
-    		"[Sun Apr 08 12:51:52 2012] [notice] Digest: done",
-    		"[Mon Jul 11 09:26:13 2011] [error] [client ::1] File does not exist: /Library/WebServer/Documents/favicon.ico",
-    	]
-
-    	for line in lines:
-    		match = re.search(pattern, line)
-    		self.assertTrue(
-    			match,
-    			"Regexp pattern '{0}' didn't match line '{1}'".format(pattern, line)
-    		)
+def test_search():
+    for line in log_lines():
+        assert search(line), "Regexp pattern '{0}' didn't match line '{1}'".format(pattern, line)
