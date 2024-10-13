cfced4e9dd04ec1da0a0f5c25197872f730aa60a
Peter Hillerström <peter.hillerstrom@gmail.com>
2024-10-13 18:38:10 +0300

    Remove Python 2.6 from classifiers

--- setup.py
+++ setup.py
@@ -61,7 +61,6 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: Unix",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Bug Tracking",
